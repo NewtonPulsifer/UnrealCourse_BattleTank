ba66009d53aec76cafff4d07d58472aabf2d3e6a
Newton Pulsifer <no@email.address>
2016-07-19 20:50:21 +0100

    After 120 - Worked out crosshair location

--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -24,5 +24,9 @@ private:
 
 	bool GetSightRayHitLocation(FVector & HitLocation) const;
 	
+	UPROPERTY(EditAnywhere)
+	float CrosshairXLocation = 0.5;
+	UPROPERTY(EditAnywhere)
+	float CrosshairYLocation = 0.33333;
 	
 };
