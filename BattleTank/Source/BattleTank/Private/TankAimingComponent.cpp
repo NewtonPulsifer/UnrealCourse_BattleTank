c2d439edde17e3f86e98353aede50715d1ee838c
Newton Pulsifer <no@email.address>
2016-07-22 18:49:46 +0100

    After 126 - Added launch speed

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -40,12 +40,12 @@ void UTankAimingComponent::TickComponent( float DeltaTime, ELevelTick TickType, 
 	// ...
 }
 
-void UTankAimingComponent::AimAt(FVector HitLocation) {
+void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed) {
 	FString OurTankName = GetOwner()->GetName();
 	FVector BarrelLocation = FVector(0);
 	if (Barrel)
 	{
 		BarrelLocation = Barrel->GetComponentLocation();
 	}
-	UE_LOG(LogTemp, Warning, TEXT("%s aiming at %s from %s"), *OurTankName, *HitLocation.ToString(), *BarrelLocation.ToString());
+	UE_LOG(LogTemp, Warning, TEXT("Firing at %f"), LaunchSpeed);
 }
